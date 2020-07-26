cd1104ad17a19f3b259c1f0ccd19075085376e33
whitewolf2019 <httyd2019@gmail.com>
2020-07-26 16:50:59 +0200

    gender_field/filter/grouping

--- Custom/demo/patient.py
+++ Custom/demo/patient.py
@@ -1,12 +1,14 @@
-from odoo import models, fields,api, _
+from odoo import models, fields, api, _
+
 
 class HospitalPatient(models.Model):
     _inherit = 'sale.order'
     demo_name = fields.Char(string='patient name')
 
+
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
-    _inherit = ['mail.thread','mail.activity.mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Patient Record'
     _rec_name = 'demo_name'
     demo_name = fields.Char(string='name', required=True)
@@ -16,8 +18,12 @@ class HospitalPatient(models.Model):
     name = fields.Char(string='test')
     name_seq = fields.Char(string='Order Reference', required=True, copy=False, readonly=True,
                            index=True, default=lambda self: _('New'))
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ], default='male', string="gender")
 
-    @ api.model
+    @api.model
     def create(self, vals):
         if vals.get('name_seq', _('New')) == _('New'):
             vals['name_seq'] = self.env['ir.sequence'].next_by_code('hospital.patient.sequence') or _('New')
