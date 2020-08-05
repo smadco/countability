85f25c7ede381802c6aa19a185d59b3e767f0541
whitewolf2019 <httyd2019@gmail.com>
2020-08-05 22:41:33 +0200

    client.py

--- Custom/countability/client.py
+++ Custom/countability/client.py
@@ -6,7 +6,7 @@ class AccClient(models.Model):
     _description = 'client record'
 
     client_name = fields.Char(string='nom et prenom or raison social', required=True, track_visibility="always")
-    num_matricul_fiscal = fields.Integer(string='numero matricul fiscal', required=True)
+    num_matricule_fiscal = fields.Integer(string='numero matricul fiscal', required=True)
     identification_unique = fields.Char(string='identification unique')
     cnss_regional = fields.Char(string='cnss regional')
     cnss_independent = fields.Char(string='cnss independent')
