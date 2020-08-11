f4434c28a53107f5184833279a1c3b0fa6efbcd4
whitewolf2019 <httyd2019@gmail.com>
2020-08-11 11:10:55 +0200

    rec_name

--- Custom/countability/models/client.py
+++ Custom/countability/models/client.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class AccClient(models.Model):
     _name = 'acc.client'
     _description = 'client record'
+    _rec_name = 'client_name'
 
     client_name = fields.Char(string='nom et prenom or raison social', required=True, track_visibility="always")
     num_matricule_fiscal = fields.Integer(string='numero matricul fiscal', required=True)
