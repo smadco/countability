a6f72a08a01b57c46b3a876144eb8c4cfc204f3e
whitewolf2019 <httyd2019@gmail.com>
2020-08-30 22:19:34 +0200

    combination

--- Custom/countability/models/declaration_mensuelle.py
+++ Custom/countability/models/declaration_mensuelle.py
@@ -5,7 +5,7 @@ from odoo.exceptions import ValidationError
 class DeclarationMensuelle(models.Model):
     _name = 'declaration.mensuelle'
     _description = 'declaration mensuelle'
-
+    # _rec_name = 'combination'
 
     @api.constrains('month')
     def check_month(self):
@@ -16,7 +16,7 @@ class DeclarationMensuelle(models.Model):
     @api.constrains('year')
     def check_year(self):
         for rec in self:
-            if rec.year >= 2030 or rec.year <= 2019:
+            if rec.year >= 2050 or rec.year <= 2019:
                 raise ValidationError(_('année incorrect.'))
 
     pay = fields.Integer(string='valeur a payé')
@@ -24,5 +24,11 @@ class DeclarationMensuelle(models.Model):
     month = fields.Integer(string='mois')
     year = fields.Integer(string='année')
     piece_comptable_ids = fields.One2many(comodel_name='piece.comptable',inverse_name='declration_mensuelle_id')
-    # client_ids = fields.One2many(comodel_name='acc.client',inverse_name='gestionnaire_id')
+    client_ids = fields.One2many(comodel_name='acc.client',inverse_name='gestionnaire_id')
+    # @api.depends('month', 'year')
+    # def _compute_fields_combination(self):
+    #     for test in self:
+    #         test.combination = test.month + ' ' + test.year
     declaration_mensuelle_id = fields.Many2one(comodel_name='Acc.client')
+    # combination = fields.Char(string='Combination', compute='_compute_fields_combination')
+
